b48f6830a06395498462175aec54a5de5679e53b
blm34 <48328783+blm34@users.noreply.github.com>
2023-12-27 23:32:37 +0000

    Added a template .py file

---
+++ template.py
@@ -0,0 +1,18 @@
+import Modules
+import sys
+
+p1 = 0
+p2 = 0
+
+text = Modules.readInput(sys.argv[1]) 
+
+for i, char in enumerate(text):
+    if char == '(':
+        p1 += 1
+    elif char == ')':
+        p1 -= 1
+    if p1 == -1 and not p2:
+        p2 = i+1
+
+print(f'Part 1: {p1}')
+print(f'Part 2: {p2}')
