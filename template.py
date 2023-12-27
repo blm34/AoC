336d184af3e7fcbb08558394a23430323340ae3a
blm34 <48328783+blm34@users.noreply.github.com>
2023-12-27 23:33:25 +0000

    Filled in the template file

--- template.py
+++ template.py
@@ -6,13 +6,7 @@ p2 = 0
 
 text = Modules.readInput(sys.argv[1]) 
 
-for i, char in enumerate(text):
-    if char == '(':
-        p1 += 1
-    elif char == ')':
-        p1 -= 1
-    if p1 == -1 and not p2:
-        p2 = i+1
+
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
