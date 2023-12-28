9a413e2c61df721851e3a55c431f441da08bd4a3
blm34 <48328783+blm34@users.noreply.github.com>
2023-12-28 22:45:39 +0000

    Solve day 6

---
+++ 6.py
@@ -0,0 +1,52 @@
+import Modules
+import sys
+
+p1 = 0
+p2 = 0
+
+text = Modules.readInput(sys.argv[1]) 
+L = text.split('\n')
+G = [list(line) for line in L]
+R = len(G)
+C = len(G[0])
+
+lights1 = [[False for c in range(1000)] for r in range(1000)]
+lights2 = [[0 for c in range(1000)] for r in range(1000)]
+
+for line in L:
+    if line.startswith('turn off'):
+        ran = line[9:]
+        start, end = ran.split(' through ')
+        start = [int(num) for num in start.split(',')]
+        end = [int(num) for num in end.split(',')]
+        for r in range(start[0], end[0]+1):
+            for c in range(start[1], end[1]+1):
+                lights1[r][c] = False
+                lights2[r][c] = max(lights2[r][c]-1, 0)
+    elif line.startswith('turn on'):
+        ran = line[8:]
+        start, end = ran.split(' through ')
+        start = [int(num) for num in start.split(',')]
+        end = [int(num) for num in end.split(',')]
+        for r in range(start[0], end[0]+1):
+            for c in range(start[1], end[1]+1):
+                lights1[r][c] = True
+                lights2[r][c] += 1
+    elif line.startswith('toggle'):
+        ran = line[7:]
+        start, end = ran.split(' through ')
+        start = [int(num) for num in start.split(',')]
+        end = [int(num) for num in end.split(',')]
+        for r in range(start[0], end[0]+1):
+            for c in range(start[1], end[1]+1):
+                lights1[r][c] = not lights1[r][c]
+                lights2[r][c] += 2
+
+p1 = sum(sum(val for val in row) for row in lights1)
+p2 = sum(sum(val for val in row) for row in lights2)
+
+print(f'Part 1: {p1}')
+print(f'Part 2: {p2}')
+
+assert p1 == 400410
+assert p2 == 15343601
