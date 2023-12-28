3cacc21749cb1bc8472f53f01ce177efe1849a6b
blm34 <48328783+blm34@users.noreply.github.com>
2023-12-28 16:45:05 +0000

    Fixed set usage in day 3

--- 3.py
+++ 3.py
@@ -12,22 +12,22 @@ move_dict = {
 
 x, y, x1, y1, x2, y2 = 0,0,0,0,0,0
 
-history1 = set((x,y))
+history1 = set(((x, y),))
 history2 = set(((x1, y1), (x2, y2)))
 
 for i, char in enumerate(text):
     dx, dy = move_dict[char]
     x += dx
     y += dy
-    history1.add((x, y),)
+    history1.add((x, y))
     if i%2 == 0:
         x1 += dx
         y1 += dy
-        history2.add((x1, y1),)
+        history2.add((x1, y1))
     else:
         x2 += dx
         y2 += dy
-        history2.add((x2, y2),)
+        history2.add((x2, y2))
 
 p1 = len(history1)
 p2 = len(history2)
