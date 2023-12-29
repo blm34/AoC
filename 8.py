f86f33d220e2b85f20c6c6e347288e91d3856b0c
blm34 <48328783+blm34@users.noreply.github.com>
2023-12-29 18:13:16 +0000

    Made day 8 more efficient

--- 8.py
+++ 8.py
@@ -10,27 +10,25 @@ L = text.split('\n')
 for line in L:
     # Part 1
     p1 += 2 # For the two quotes on either end
+    p2 += 2 # For the two quotes on either end
     i = 0
     while i < len(line):
-        if line[i] == '\\':
-            if line[i+1] == '\\':
+        if line[i] == '\"': # If " then a \ added for part 2
+            p2 += 1
+        elif line[i] == '\\': # If \ then a \ added for part 2
+            p2 += 1
+            if line[i+1] == '\\': # If next char \ then \ removed in part 1 and another added in part 2
                 p1 += 1
+                p2 += 1
                 i += 1
-            elif line[i+1] == '\"':
+            elif line[i+1] == '\"': # If next char " then \ removed in part 1 and added in part 2
                 p1 += 1
+                p2 += 1
                 i += 1
-            elif line[i+1] == 'x':
+            elif line[i+1] == 'x': # If next char # then 4 chars become 1 in part 1 and next 3 char irrelevant for part 2
                 p1 += 3
                 i += 3
         i += 1
-
-    # Part 2
-    p2 += 2 # For the two quotes on either end
-    for char in line:
-        if char == '\"':
-            p2 += 1
-        elif char == '\\':
-            p2 += 1
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
