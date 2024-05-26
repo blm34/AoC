e70ffcee0401400fe7c6bddd94034054062e1e25
blm34 <48328783+blm34@users.noreply.github.com>
2024-05-26 13:08:25 +0100

    Solve day 19

--- 19.py
+++ 19.py
@@ -8,37 +8,52 @@ text = Modules.readInput("19.txt")
 
 replacements_text, molecule = text.split('\n\n')
 
-replacements = dict()
-backwards_replacements = dict()
+replacements = list()
 for line in replacements_text.split('\n'):
     m1, m2 = line.split(' => ')
-    if m1 not in replacements:
-        replacements[m1] = list()
-    replacements[m1].append(m2)
-    if m2 not in backwards_replacements:
-        backwards_replacements[m2] = list()
-    backwards_replacements[m2].append(m1)
+    replacements.append((m1, m2))
 
 
 def step(molecule, replacements):
     unique = set()
-    i = 0
     j = -1
     while j+1 < len(molecule):
+        # Find start of next element
         i = j = j+1
+        # Find end of element
         while j+1 < len(molecule) and molecule[j+1] == molecule[j+1].lower():
             j += 1
         element = molecule[i:j+1]
-        if element not in replacements:
-            continue
-        for rep in replacements[element]:
-            new_molecule = molecule[:i] + rep + molecule[j+1:]
-            unique.add(new_molecule)
+        for m1, m2 in replacements:
+            if element == m1:
+                new_molecule = molecule[:i] + m2 + molecule[j+1:]
+                unique.add(new_molecule)
     return unique
 
 
-p1 = len(step(molecule, replacements))
+def step_count(molecule):
+    j = -1
+    count = -1
+    while j+1 < len(molecule):
+        # find start of next element
+        i = j = j+1
+        # Find end of element
+        while j+1 < len(molecule) and molecule[j+1] == molecule[j+1].lower():
+            j += 1
+        element = molecule[i:j+1]
+        if element == 'Rn':
+            pass
+        elif element == 'Ar':
+            pass
+        elif element == 'Y':
+            count -= 1
+        else:
+            count += 1
+    return count
 
+
+p1 = len(step(molecule, replacements))
+p2 = step_count(molecule)
 
 END_TIME = time.time()
 RUN_TIME = END_TIME - START_TIME
