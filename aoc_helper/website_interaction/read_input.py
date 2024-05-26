06f3c9c1b71f44865b566d02bf9942a6e38be146
blm34 <48328783+blm34@users.noreply.github.com>
2024-05-26 22:41:38 +0100

    Download input from website automatically

--- aoc_helper/website_interaction/read_input.py
+++ aoc_helper/website_interaction/read_input.py
@@ -1,13 +1,46 @@
-def read_input(file: str) -> str:
+from pathlib import Path
+
+import browser_cookie3 as bc3
+import requests
+
+
+def read_input(day: int, year: int) -> str:
     """
     Opens the given file. Typically a puzzle input
 
     Args:
-        file: The file to open
+        day: Load the puzzle input for this day.
+        year: Load the puzzle input for this year.
 
     Returns:
         The contents of the file
     """
-    with open(f'input_files\\{file}', 'r') as file:
-        text = file.read().strip()
-    return text
+    filepath = Path.joinpath(Path("..").resolve(), "input_files", f"{day}.txt")
+
+    if not filepath.exists():
+        puzzle_input = download_input(day, year)
+        with open(filepath, 'w') as file:
+            file.write(puzzle_input)
+
+    else:
+        with open(filepath, 'r') as file:
+            puzzle_input = file.read().strip()
+
+    return puzzle_input
+
+
+def download_input(day: int, year: int) -> str:
+    """
+    Download the puzzle input for the given day and year from the advent of code website, and return as a string.
+
+    Args:
+        day: Load the puzzle input for this day.
+        year: Load the puzzle input for this year.
+
+    Returns:
+        The puzzle input for the given day
+    """
+    cookies = bc3.firefox(domain_name=".adventofcode.com")
+    r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies)
+    return r.text
+
