d69ff1c79dc9531a75a6ae2b894ed89cca3854a4
blm34 <48328783+blm34@users.noreply.github.com>
2024-05-28 20:44:37 +0100

    Retrieve cookies from Chrome if not in firefox

--- aoc_helper/website_interaction/read_input.py
+++ aoc_helper/website_interaction/read_input.py
@@ -41,6 +41,9 @@ def download_input(day: int, year: int) -> str:
         The puzzle input for the given day
     """
     cookies = bc3.firefox(domain_name=".adventofcode.com")
+    if not (".adventofcode.com" in str(cookies)):
+        input("Cookie not in Firefox. Temporarily Close chrome then press enter to continue")
+        cookies = bc3.chrome(domain_name=".adventofcode.com")
+        print("You may now reopen Chrome")
     r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies)
     return r.text
-
