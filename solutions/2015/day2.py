26e5b701b6ae7426d3a140e6be9daaa0ebbde98f
blm34 <48328783+blm34@users.noreply.github.com>
2024-06-01 19:38:33 +0100

    Update day 2 for new communicator

--- solutions/2015/day2.py
+++ solutions/2015/day2.py
@@ -1,21 +1,36 @@
+import time
+
 import aoc_helper
-import sys
 
-p1 = 0
-p2 = 0
+DAY = 2
+YEAR = 2015
+START_TIME = time.time()
 
-text = aoc_helper.read_input("2.txt")
 
-for line in text.split('\n'):
-    dims = [int(x) for x in line.split('x')]
-    areas = [dims[i]*dims[(i+1)%3] for i in range(3)]
-    volume = aoc_helper.prod(dims)
-    min_circ = (sum(dims)-max(dims))*2
-    p1 += 2 * sum(areas) + min(areas)
-    p2 += volume + min_circ
+@aoc_helper.communicator(YEAR, DAY, 1)
+def p1(input_text):
+    ans = 0
+    for line in input_text.split('\n'):
+        dims = [int(x) for x in line.split('x')]
+        areas = [dims[i] * dims[(i + 1) % 3] for i in range(3)]
+        ans += 2 * sum(areas) + min(areas)
+    return ans
 
-print(f'Part 1: {p1}')
-print(f'Part 2: {p2}')
 
-assert p1 == 1588178
-assert p2 == 3783758
+@aoc_helper.communicator(YEAR, DAY, 2)
+def p2(input_text):
+    ans = 0
+    for line in input_text.split('\n'):
+        dims = [int(x) for x in line.split('x')]
+        volume = aoc_helper.prod(dims)
+        min_circ = (sum(dims) - max(dims)) * 2
+        ans += volume + min_circ
+    return ans
+
+
+if __name__ == "__main__":
+    p1_res = p1()
+    p2_res = p2()
+
+    END_TIME = time.time()
+    aoc_helper.print_results(p1_res, p2_res, END_TIME - START_TIME)
