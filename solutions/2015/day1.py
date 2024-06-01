9aff1ebdfb31380107ad2c1d5a056644057c24d8
blm34 <48328783+blm34@users.noreply.github.com>
2024-06-01 19:30:14 +0100

    Update day 1 for new communicator

--- solutions/2015/day1.py
+++ solutions/2015/day1.py
@@ -1,21 +1,37 @@
+import time
+
 import aoc_helper
-import sys
 
-p1 = 0
-p2 = 0
+DAY = 1
+YEAR = 2015
+START_TIME = time.time()
 
-text = aoc_helper.read_input(sys.argv[1])
+@aoc_helper.communicator(YEAR, DAY, 1)
+def p1(input_text):
+    ans = 0
+    for i, char in enumerate(input_text):
+        if char == '(':
+            ans += 1
+        elif char == ')':
+            ans -= 1
+    return ans
 
-for i, char in enumerate(text):
-    if char == '(':
-        p1 += 1
-    elif char == ')':
-        p1 -= 1
-    if p1 == -1 and not p2:
-        p2 = i+1
 
-print(f'Part 1: {p1}')
-print(f'Part 2: {p2}')
+@aoc_helper.communicator(YEAR, DAY, 2)
+def p2(input_text):
+    floor = 0
+    for i, char in enumerate(input_text):
+        if char == '(':
+            floor += 1
+        elif char == ')':
+            floor -= 1
+        if floor == -1:
+            return i+1
 
-assert p1 == 280
-assert p2 == 1797
+
+if __name__ == "__main__":
+    p1_res = p1()
+    p2_res = p2()
+
+    END_TIME = time.time()
+    aoc_helper.print_results(p1_res, p2_res, END_TIME - START_TIME)
