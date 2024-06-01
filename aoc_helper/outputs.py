4825268fe07d30d10316c51668f385795192d117
blm34 <48328783+blm34@users.noreply.github.com>
2024-06-01 22:20:12 +0100

    Output parts 1 and 2 separately

--- aoc_helper/outputs.py
+++ aoc_helper/outputs.py
@@ -1,14 +1,18 @@
-def print_results(p1, p2, time=None):
-    output = f"Part 1: {p1[0]:<10}"
-    output += "✅" if p1[1] else "❌"
-    output += "\n"
-
-    output += f"Part 2: {p2[0]:<10}"
-    output += "✅" if p2[1] else "❌"
-    output += "\n"
+def print_results(result, time=None, year=None, day=None, part=None):
+    output = ""
+    if year is not None:
+        output += f"{year} "
+    if day is not None:
+        output += f"{day:02d} "
+    if part is not None:
+        output += f"Part {part} "
+    if output != "":
+        output = output.strip() + ": "
+    output += f"{result[0]:<10}"
+    output += " ✅ " if result[1] else " ❌ "
 
     if time is not None:
-        output += "\nRun time: "
+        output += "Run time: "
         output += f"{1000 * time:.3f} ms" if time < 1 else f"{time:.1f} s"
 
     print(output)
