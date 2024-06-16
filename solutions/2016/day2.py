05ad1403281459fbc63619f186888ed215287f7f
blm34 <48328783+blm34@users.noreply.github.com>
2024-06-16 19:15:06 +0100

    Solve 2016 day 2

---
+++ solutions/2016/day2.py
@@ -0,0 +1,73 @@
+import aoc_helper
+
+DAY = 2
+YEAR = 2016
+
+MOVES = {'U': (0, 1),
+         'D': (0, -1),
+         'R': (1, 0),
+         'L': (-1, 0)}
+
+
+def parse_input(input_text):
+    L = input_text.split('\n')
+    return L
+
+
+def loc_to_num_p1(loc):
+    col, row = loc
+    return (col + 1) + (1 - row) * 3 + 1
+
+
+def loc_to_num_p2(loc):
+    pad = [['_', '_', '1', '_', '_'],
+           ['_', '2', '3', '4', '_'],
+           ['5', '6', '7', '8', '9'],
+           ['_', 'A', 'B', 'C', '_'],
+           ['_', '_', 'D', '_', '_']]
+    x, y = loc
+    row = 2 - y
+    col = 2 + x
+    return pad[row][col]
+
+
+@aoc_helper.communicator(YEAR, DAY, 1)
+def p1(input_text):
+    L = parse_input(input_text)
+    loc = (0, 0)
+    ans = ""
+
+    for line in L:
+        for char in line:
+            x = loc[0] + MOVES[char][0]
+            y = loc[1] + MOVES[char][1]
+            if abs(x) <= 1 and abs(y) <= 1:
+                loc = (x, y)
+
+        ans += str(loc_to_num_p1(loc))
+    return ans
+
+
+@aoc_helper.communicator(YEAR, DAY, 2)
+def p2(input_text):
+    L = parse_input(input_text)
+    loc = (-2, 0)
+    ans = ""
+
+    for line in L:
+        for char in line:
+            x = loc[0] + MOVES[char][0]
+            y = loc[1] + MOVES[char][1]
+            if abs(x) + abs(y) <= 2:
+                loc = (x, y)
+
+        ans += loc_to_num_p2(loc)
+    return ans
+
+
+if __name__ == "__main__":
+    p1_res = p1()
+    aoc_helper.print_results(p1_res, part=1)
+
+    p2_res = p2()
+    aoc_helper.print_results(p2_res, part=2)
