fdbe050d10ca2ae21829727898b95a7432fa813d
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-06 22:53:04 +0000

    Allow for incomplete solutions when running all solutions in runner

--- solutions/runner.py
+++ solutions/runner.py
@@ -7,10 +7,10 @@ from aoc_helper import print_results
 
 
 def get_file_path(year, day):
-    return Path(str(year)).joinpath(f"day{day}.py")
+    return Path(str(year)) / f"day{day}.py"
 
 
-if __name__ == "__main__":
+def main():
     stars = 0
     total_runtime = 0
 
@@ -31,17 +31,25 @@ if __name__ == "__main__":
             spec.loader.exec_module(module)
 
             # Run part 1
-            p1_res = module.p1()
-            print_results(p1_res, year=year, day=day, part=1)
-            stars += 1 if p1_res.correct else 0
-            stars += 1 if p1_res.correct and day == 25 else 0
-            total_runtime += p1_res.time
+            try:
+                p1_res = module.p1()
+            except:
+                print(f"{year} {day:02d} Part 1: Not Implemented", file=sys.stderr)
+            else:
+                print_results(p1_res, year=year, day=day, part=1)
+                stars += 1 if p1_res.correct else 0
+                stars += 1 if p1_res.correct and day == 25 else 0
+                total_runtime += p1_res.time
 
             # Run part 2
-            p2_res = module.p2()
-            print_results(p2_res, year=year, day=day, part=2)
-            stars += 1 if p2_res.correct else 0
-            total_runtime += p2_res.time
+            try:
+                p2_res = module.p2()
+            except:
+                print(f"{year} {day:02d} Part 2: Not Implemented", file=sys.stderr)
+            else:
+                print_results(p2_res, year=year, day=day, part=2)
+                stars += 1 if p2_res.correct else 0
+                total_runtime += p2_res.time
 
     max_stars = (current_year - 2015) * 50
     if current_month == 12:
@@ -49,3 +57,7 @@ if __name__ == "__main__":
 
     print(f"\nEarned {stars}* out of {max_stars}* ({100 * stars / max_stars:.1f}%)")
     print(f"\nTotal runtime: {total_runtime:.1f} s\nAverage runtime: {1000 * total_runtime / stars:.0f} ms")
+
+
+if __name__ == "__main__":
+    main()
