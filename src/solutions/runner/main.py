6f86cef1e51e6b64570e2302ed34f77000c2d4bb
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-15 19:25:12 +0000

    Write main function for the runner

---
+++ src/solutions/runner/main.py
@@ -0,0 +1,22 @@
+from .cli import parse_args
+from .runner import run_one_day, run_years
+from .stats import show_stars, show_times
+
+
+def main():
+    args = parse_args()
+
+    if args.day != 0:  # Run a single day
+        results = run_one_day(args)
+    else:  # Run whole years
+        results = run_years(args)
+
+    if args.stars:
+        show_stars(results, args)
+
+    if args.repeats > 1:
+        show_times(results, args)
+
+
+if __name__ == "__main__":
+    main()
