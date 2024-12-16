0a4471a03dd3de81c13d9e29a23b54d99a15d533
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-16 20:13:47 +0000

    Improve speed of 2016 day 5

--- src/solutions/2016/day5.py
+++ src/solutions/2016/day5.py
@@ -1,4 +1,5 @@
 import hashlib
+import itertools
 
 import aoc_helper
 
@@ -9,28 +10,32 @@ YEAR = 2016
 @aoc_helper.communicator(YEAR, DAY, 1)
 def p1(input_text):
     password = ""
-    num = -1
-    while len(password) < 8:
-        num += 1
-        hash = hashlib.md5(f"{input_text}{num}".encode()).hexdigest()
-        if hash[:5] == "00000":
-            password += hash[5]
-    return password
+    hash_start = hashlib.md5(str(input_text).encode('utf-8'))
+    for num in itertools.count():
+        updated_hash = hash_start.copy()
+        updated_hash.update(str(num).encode('utf-8'))
+        hash_bytes = updated_hash.digest()
+        if (not hash_bytes[0]) and (not hash_bytes[1]) and (not (hash_bytes[2] >> 4)):
+            password += hex(hash_bytes[2])[2]
+            if len(password) == 8:
+                return password
 
 
 @aoc_helper.communicator(YEAR, DAY, 2)
 def p2(input_text):
-    password: list[None | str] = [None for _ in range(8)]
-    num = -1
-    while any(digit is None for digit in password):
-        num += 1
-        hash = hashlib.md5(f"{input_text}{num}".encode()).hexdigest()
-        if hash[:5] == "00000":
-            loc = int(hash[5], 16)
-            val = hash[6]
+    password = [None for _ in range(8)]
+    hash_start = hashlib.md5(str(input_text).encode('utf-8'))
+    for num in itertools.count():
+        updated_hash = hash_start.copy()
+        updated_hash.update(str(num).encode('utf-8'))
+        hash_bytes = updated_hash.digest()
+        if (not hash_bytes[0]) and (not hash_bytes[1]) and (not (hash_bytes[2] >> 4)):
+            loc = hash_bytes[2] & 15
+            val = hash_bytes[3] >> 4
             if loc < 8 and password[loc] is None:
-                password[loc] = val
-    return ''.join(password)
+                password[loc] = str(val)
+                if all(digit is not None for digit in password):
+                    return ''.join(password)
 
 
 if __name__ == "__main__":
