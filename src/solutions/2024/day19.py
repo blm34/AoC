a68936756baaf19a42cf57097f841004124c3e88
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-19 22:25:55 +0000

    Speed up 2024 day 19 using a trie

--- src/solutions/2024/day19.py
+++ src/solutions/2024/day19.py
@@ -6,19 +6,52 @@ DAY = 19
 YEAR = 2024
 
 
+class TrieNode:
+    def __init__(self):
+        self.end = False
+        self.children = dict()
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, value):
+        node = self.root
+        for char in value:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end = True
+
+    def valid_prefixes(self, string):
+        node = self.root
+        prefix = ""
+        for char in string:
+            if char not in node.children:
+                return
+            prefix += char
+            node = node.children[char]
+            if node.end:
+                yield prefix
+
+
 def parse_input(input_text):
     towels, patterns = input_text.split("\n\n")
     towels = tuple(towels.split(", "))
     patterns = patterns.split("\n")
-    return towels, patterns
+    towels_trie = Trie()
+    for towel in towels:
+        towels_trie.add(towel)
+    return towels_trie, patterns
 
 
 @cache
 def possible_pattern(pattern, towels):
     if pattern == "":
         return True
-    for towel in towels:
-        if pattern.startswith(towel) and possible_pattern(pattern[len(towel):], towels):
+    for towel in towels.valid_prefixes(pattern):
+        if pattern.startswith(towel) and possible_pattern(pattern.removeprefix(towel), towels):
             return True
     return False
 
@@ -28,9 +61,9 @@ def count_pattern(pattern, towels):
     if pattern == "":
         return 1
     count = 0
-    for towel in towels:
+    for towel in towels.valid_prefixes(pattern):
         if pattern.startswith(towel):
-            count += count_pattern(pattern[len(towel):], towels)
+            count += count_pattern(pattern.removeprefix(towel), towels)
     return count
 
 
