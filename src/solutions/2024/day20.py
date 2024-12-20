fe1fcde4b5c9e256bdaf996be7a66232de9c6d24
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-20 16:38:23 +0000

    Speed up 2024 day 20

--- src/solutions/2024/day20.py
+++ src/solutions/2024/day20.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 import aoc_helper
 
 DAY = 20
@@ -7,16 +9,6 @@ dirs = ((-1, 0),
         (0, 1),
         (1, 0),
         (0, -1))
-
-dirs2 = ((-2, 0),
-         (-1, 1),
-         (0, 2),
-         (1, 1),
-         (2, 0),
-         (1, -1),
-         (0, -2),
-         (-1, -1))
-
 
 def parse_input(input_text):
     grid = input_text.split('\n')
@@ -31,11 +23,24 @@ def parse_input(input_text):
     return grid, start, end
 
 
-def get_path(grid, start, end):
-    path = {start: 0}
-    prev = start
+def get_path_list(grid, start, end) -> list[tuple[int, int]]:
+    path = [None, start]
+    while path[-1] != end:
+        r, c = path[-1]
+        for dr, dc in dirs:
+            rr = r + dr
+            cc = c + dc
+            if grid[rr][cc] != "#" and (rr, cc) != path[-2]:
+                path.append((rr, cc))
+                break
+    return tuple(path[1:])
+
+
+def get_path_dict(grid, start, end) -> dict[tuple[int, int], int]:
     prev_prev = None
+    prev = start
     dist = 0
+    path = {start: dist}
     while prev != end:
         dist += 1
         r, c = prev
@@ -50,16 +55,37 @@ def get_path(grid, start, end):
     return path
 
 
+def get_cheat_count(path, max_cheat_len):
+    count = 0
+    for i1 in range(len(path) - 102):
+        i2 = i1 + 102
+        r1, c1 = path[i1]
+        while i2 < len(path):
+            r2, c2 = path[i2]
+            dr = abs(r1 - r2)
+            dc = abs(c2 - c1)
+            if dr + dc <= max_cheat_len:
+                saving = i2 - i1 - dr - dc
+                if saving >= 100:
+                    count += 1
+                    i2 += 1
+                else:
+                    i2 += (100 - saving) // 2
+            else:
+                i2 += dr + dc - max_cheat_len
+    return count
+
+
 @aoc_helper.communicator(YEAR, DAY, 1)
 def p1(input_text):
     grid, start, end = parse_input(input_text)
-    path = get_path(grid, start, end)
+    path = get_path_dict(grid, start, end)
 
     count = 0
     for (r, c), dist in path.items():
-        for dr, dc in dirs2:
-            rr = r + dr
-            cc = c + dc
+        for dr, dc in dirs:
+            rr = r + dr*2
+            cc = c + dc*2
             if (rr, cc) in path:
                 new_dist = path[(rr, cc)]
                 if new_dist - dist - 2 >= 100:
@@ -71,22 +97,8 @@ def p1(input_text):
 @aoc_helper.communicator(YEAR, DAY, 2)
 def p2(input_text):
     grid, start, end = parse_input(input_text)
-    path = get_path(grid, start, end)
-
-    count = 0
-    for pos1 in path:
-        r1, c1 = pos1
-        for dr in range(-20, 21):
-            for dc in range(-20, 21):
-                dist = abs(dr) + abs(dc)
-                if dist > 20:
-                    continue
-                r2 = r1 + dr
-                c2 = c1 + dc
-                if (r2, c2) in path and path[(r2, c2)] - path[pos1] - dist >= 100:
-                    count += 1
-    return count
-
+    path = get_path_list(grid, start, end)
+    return get_cheat_count(path, 20)
 
 if __name__ == "__main__":
     p1_res = p1()
