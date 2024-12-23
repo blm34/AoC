33f73708c4b8d904dc0c05c30c85ee6fd2fcb8b0
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-23 12:18:31 +0000

    Speed up 2016 day 5

--- src/solutions/2016/day5.py
+++ src/solutions/2016/day5.py
@@ -7,38 +7,24 @@ DAY = 5
 YEAR = 2016
 
 
-def p1(input_text):
-    password = ""
-    hash_start = hashlib.md5(str(input_text).encode('utf-8'))
-    for num in itertools.count():
-        updated_hash = hash_start.copy()
-        updated_hash.update(str(num).encode('utf-8'))
-        hash_bytes = updated_hash.digest()
-        if (not hash_bytes[0]) and (not hash_bytes[1]) and (not (hash_bytes[2] >> 4)):
-            password += hex(hash_bytes[2])[2]
-            if len(password) == 8:
-                return password
-
-
-def p2(input_text):
-    password = [None for _ in range(8)]
-    hash_start = hashlib.md5(str(input_text).encode('utf-8'))
-    for num in itertools.count():
-        updated_hash = hash_start.copy()
-        updated_hash.update(str(num).encode('utf-8'))
-        hash_bytes = updated_hash.digest()
-        if (not hash_bytes[0]) and (not hash_bytes[1]) and (not (hash_bytes[2] >> 4)):
-            loc = hash_bytes[2] & 15
-            if loc < 8 and password[loc] is None:
-                val = hash_bytes[3] >> 4
-                password[loc] = hex(val)[2]
-                if all(digit is not None for digit in password):
-                    return ''.join(password)
-
-
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
-    return p1(input_text), p2(input_text)
+    p1 = ""
+    p2 = [None for _ in range(8)]
+    hash_start = hashlib.md5(str(input_text).encode('utf-8'))
+    for num in itertools.count():
+        updated_hash = hash_start.copy()
+        updated_hash.update(str(num).encode('utf-8'))
+        hash_bytes = updated_hash.digest()
+        if (not hash_bytes[0]) and (not hash_bytes[1]) and (not (hash_bytes[2] >> 4)):
+            if len(p1) < 8:
+                p1 += hex(hash_bytes[2])[2]
+            loc = hash_bytes[2] & 15
+            if loc < 8 and p2[loc] is None:
+                val = hash_bytes[3] >> 4
+                p2[loc] = hex(val)[2]
+                if all(digit is not None for digit in p2):
+                    return p1, ''.join(p2)
 
 
 if __name__ == "__main__":
