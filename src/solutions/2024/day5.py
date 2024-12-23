100e6b598ba9d6fb0c577d2292c9b8d57c3460db
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-23 23:02:29 +0000

    Speed up 2024 day 5

--- src/solutions/2024/day5.py
+++ src/solutions/2024/day5.py
@@ -1,4 +1,5 @@
-import re
+from collections import defaultdict
+from functools import cmp_to_key
 
 import aoc_helper
 
@@ -7,72 +8,51 @@ YEAR = 2024
 
 
 def ordered(update, rules) -> bool:
-    for first, second in rules:
-        if first not in update or second not in update:
-            continue
-        first_index = update.index(first)
-        second_index = update.index(second)
-        if second_index < first_index:
+    for i in range(len(update) - 1):
+        if update[i+1] not in rules[update[i]]:
             return False
     return True
 
 
 def sort_update(update, rules) -> list[int]:
-    valid_rules = []
-    for rule in rules:
-        if rule[0] in update and rule[1] in update:
-            valid_rules.append(rule)
+    def comparator(first, second):
+        if second in rules[first]:
+            return 1
+        if first in rules[second]:
+            return -1
+        return 0
 
-    while not ordered(update, valid_rules):
-        for first, second in valid_rules:
-            first_index = update.index(first)
-            second_index = update.index(second)
-            if second_index < first_index:
-                update[first_index], update[second_index] = update[second_index], update[first_index]
-
-    return update
+    return sorted(update, key=cmp_to_key(comparator))
 
 
 def parse_input(input_text):
-    rules, updates = input_text.split("\n\n")
+    rules_text, updates = input_text.split("\n\n")
 
-    rules = re.findall(r"^(\d+)\|(\d+)$", rules, flags=re.MULTILINE)
-    rules = tuple(tuple(map(int, rule)) for rule in rules)
+    rules = defaultdict(set)  # key=page, value=set of pages that must come after key
+    for rule in rules_text.splitlines():
+        first, second = rule.split("|")
+        rules[int(first)].add(int(second))
 
-    updates = [list(map(int, update.split(","))) for update in updates.split("\n")]
+    updates = [list(map(int, update.split(","))) for update in updates.splitlines()]
 
     return rules, updates
 
 
-def p1(input_text):
-    rules, updates = parse_input(input_text)
-
-    result = 0
-    for update in updates:
-        if ordered(update, rules):
-            middle_index = len(update) // 2
-            result += update[middle_index]
-    return result
-
-
-def p2(input_text):
-    rules, updates = parse_input(input_text)
-
-    result = 0
-    for update in updates:
-        if ordered(update, rules):
-            continue
-
-        sorted_update = sort_update(update, rules)
-        middle_index = len(sorted_update) // 2
-        result += sorted_update[middle_index]
-
-    return result
-
-
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
-    return p1(input_text), p2(input_text)
+    rules, updates = parse_input(input_text)
+
+    p1 = 0
+    p2 = 0
+
+    for update in updates:
+        if ordered(update, rules):
+            p1 += update[len(update) // 2]
+        else:
+            sorted_update = sort_update(update, rules)
+            p2 += sorted_update[len(sorted_update) // 2]
+
+    return p1, p2
 
 
 if __name__ == "__main__":
