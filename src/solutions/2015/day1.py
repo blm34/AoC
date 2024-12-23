7f096d3d2fc5c491fb158908db9470ad19cfe030
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-23 15:20:39 +0000

    Speed up 2015 day 1

--- src/solutions/2015/day1.py
+++ src/solutions/2015/day1.py
@@ -3,30 +3,19 @@ import aoc_helper
 DAY = 1
 YEAR = 2015
 
-def p1(input_text):
-    ans = 0
-    for i, char in enumerate(input_text):
-        if char == '(':
-            ans += 1
-        elif char == ')':
-            ans -= 1
-    return ans
-
-
-def p2(input_text):
-    floor = 0
-    for i, char in enumerate(input_text):
-        if char == '(':
-            floor += 1
-        elif char == ')':
-            floor -= 1
-        if floor == -1:
-            return i+1
-
 
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
-    return p1(input_text), p2(input_text)
+    floor = 0
+    p2 = None
+    for i, char in enumerate(input_text):
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+        if p2 is None and floor == -1:
+            p2 = i+1
+    return floor, p2
 
 
 if __name__ == "__main__":
