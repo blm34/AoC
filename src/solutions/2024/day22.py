fc249454394709906b9f9be7fab3925fba58ab6a
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-23 23:40:11 +0000

    Speed up 2024 day 22

--- src/solutions/2024/day22.py
+++ src/solutions/2024/day22.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 import aoc_helper
 
 DAY = 22
@@ -16,21 +14,25 @@ def step(num):
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
     nums = map(int, input_text.split('\n'))
-    p1 = 0
 
-    totals = defaultdict(int)
+    p1 = 0
+    p2 = 0
+
+    totals = [0 for _ in range(19**4 - 1)]
     for num in nums:
         seen = set()
         sequence = 0
         for i in range(2000):
             last_price = num % 10
             num = step(num)
-            sequence = (sequence * 20 + (num % 10 - last_price)) % 20**4
+            sequence = (sequence * 19 + (num % 10 - last_price + 9)) % 19**4
             if i >= 3 and sequence not in seen:
                 seen.add(sequence)
                 totals[sequence] += num % 10
+                if totals[sequence] > p2:
+                    p2 = totals[sequence]
         p1 += num
-    return p1, max(totals.values())
+    return p1, p2
 
 
 if __name__ == "__main__":
