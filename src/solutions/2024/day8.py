445040defe65d2f1414e2bfb6e894792c96fd2e7
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-24 19:55:02 +0000

    Speed up 2024 day 8

--- src/solutions/2024/day8.py
+++ src/solutions/2024/day8.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import aoc_helper
 
 DAY = 8
@@ -5,68 +7,53 @@ YEAR = 2024
 
 
 def parse_input(input_text):
-    L = input_text.split('\n')
-    R = len(L)
-    C = len(L[0])
+    lines = input_text.splitlines()
+    height = len(lines)
+    width = len(lines[0])
 
-    ants = dict()
-    for r in range(R):
-        for c in range(C):
-            val = L[r][c]
+    ants = defaultdict(list)
+    for r in range(height):
+        for c in range(width):
+            val = lines[r][c]
             if val != '.':
-                if val not in ants:
-                    ants[val] = list()
                 ants[val].append((r, c))
-    return ants, R, C
-
-
-def p1(input_text):
-    ants, height, width = parse_input(input_text)
-    nodes = set()
-    for char in ants:
-        for i in range(len(ants[char])-1):
-            for j in range(i+1, len(ants[char])):
-                ant1 = ants[char][i]
-                ant2 = ants[char][j]
-
-                dr = ant1[0] - ant2[0]
-                dc = ant1[1] - ant2[1]
-
-                if 0 <= ant1[0] + dr < height and 0 <= ant1[1] + dc < width:
-                    nodes.add((ant1[0] + dr, ant1[1] + dc))
-                if 0 <= ant2[0] - dr < height and 0 <= ant2[1] - dc < width:
-                    nodes.add((ant2[0] - dr, ant2[1] - dc))
-    return len(nodes)
-
-
-def p2(input_text):
-    ants, height, width = parse_input(input_text)
-    nodes = set()
-    for char in ants:
-        for i in range(len(ants[char])-1):
-            for j in range(i+1, len(ants[char])):
-                ant1 = ants[char][i]
-                ant2 = ants[char][j]
-
-                dr = ant1[0] - ant2[0]
-                dc = ant1[1] - ant2[1]
-
-                new_node = ant1
-                while 0 <= new_node[0] < height and 0 <= new_node[1] < width:
-                    nodes.add(new_node)
-                    new_node = (new_node[0] + dr, new_node[1] + dc)
-
-                new_node = ant2
-                while 0 <= new_node[0] < height and 0 <= new_node[1] < width:
-                    nodes.add(new_node)
-                    new_node = (new_node[0] - dr, new_node[1] - dc)
-
-    return len(nodes)
+    return ants, height, width
 
 
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
-    return p1(input_text), p2(input_text)
+    ants, height, width = parse_input(input_text)
+
+    p1_nodes = set()
+    p2_nodes = set()
+
+    for char in ants:
+        for i in range(len(ants[char])-1):
+            for j in range(i+1, len(ants[char])):
+                ant1 = ants[char][i]
+                ant2 = ants[char][j]
+
+                dr = ant1[0] - ant2[0]
+                dc = ant1[1] - ant2[1]
+
+                # Part 1
+                if 0 <= ant1[0] + dr < height and 0 <= ant1[1] + dc < width:
+                    p1_nodes.add((ant1[0] + dr, ant1[1] + dc))
+                if 0 <= ant2[0] - dr < height and 0 <= ant2[1] - dc < width:
+                    p1_nodes.add((ant2[0] - dr, ant2[1] - dc))
+
+                # Part 2
+                new_node = ant1
+                while 0 <= new_node[0] < height and 0 <= new_node[1] < width:
+                    p2_nodes.add(new_node)
+                    new_node = (new_node[0] + dr, new_node[1] + dc)
+
+                new_node = ant2
+                while 0 <= new_node[0] < height and 0 <= new_node[1] < width:
+                    p2_nodes.add(new_node)
+                    new_node = (new_node[0] - dr, new_node[1] - dc)
+
+    return len(p1_nodes), len(p2_nodes)
 
 
 if __name__ == "__main__":
