70c1289424c1051a11938628637ab1b9eced97cf
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-27 18:43:02 +0000

    Speed up 2024 day 12

--- src/solutions/2024/day12.py
+++ src/solutions/2024/day12.py
@@ -14,95 +14,60 @@ corners = ((-1, 1),
            (-1, -1))
 
 
-def parse_input(input_text):
-    L = input_text.split('\n')
-    G = [list(line) for line in L]
-    R = len(G)
-    C = len(G[0])
-    return None
+@aoc_helper.communicator(YEAR, DAY)
+def solve(input_text):
+    grid = input_text.splitlines()
+    R = len(grid)
+    C = len(grid[0])
 
-
-def p1(input_text):
-    G = input_text.split('\n')
-    R = len(G)
-    C = len(G[0])
+    p1 = 0
+    p2 = 0
 
     seen = [[False for _ in range(C)] for _ in range(R)]
-    cost = 0
 
     for sr in range(R):
         for sc in range(C):
             if seen[sr][sc]:
                 continue
+            letter = grid[sr][sc]
+
             area = 0
             perimeter = 0
+            double_sides = 0
+
             stack = [(sr, sc)]
             seen[sr][sc] = True
             while stack:
                 r, c = stack.pop()
                 area += 1
+                # Look for adjacent similar letters
                 for dr, dc in directions:
                     rr = r + dr
                     cc = c + dc
-                    if 0 <= rr < R and 0 <= cc < C and G[rr][cc] == G[sr][sc]:
+                    if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == letter:
                         if not seen[rr][cc]:
                             stack.append((rr, cc))
                             seen[rr][cc] = True
                     else:
                         perimeter += 1
-            cost += area * perimeter
-    return cost
 
-
-def p2(input_text):
-    G = input_text.split('\n')
-    R = len(G)
-    C = len(G[0])
-
-    seen = [[False for _ in range(C)] for _ in range(R)]
-    cost = 0
-
-    for sr in range(R):
-        for sc in range(C):
-            if seen[sr][sc]:
-                continue
-            letter = G[sr][sc]
-            area = 0
-            double_sides = 0
-            stack = [(sr, sc)]
-            seen[sr][sc] = True
-            while stack:
-                r, c = stack.pop()
-
-                # Look for adjacent similar letters
-                for dr, dc in directions:
-                    rr = r + dr
-                    cc = c + dc
-                    if 0 <= rr < R and 0 <= cc < C and G[rr][cc] == letter and not seen[rr][cc]:
-                        stack.append((rr, cc))
-                        seen[rr][cc] = True
-
-                # Calculate effect of area and sides of current cell
-                area += 1
+                # Calculate the of current cell on sides
                 for dr, dc in corners:
                     rr = r + dr
                     cc = c + dc
-                    corner = G[rr][cc] == letter if 0 <= rr < R and 0 <= cc < C else 0
-                    edges = G[rr][c] == letter if 0 <= rr < R and 0 <= c < C else 0
-                    edges += G[r][cc] == letter if 0 <= r < R and 0 <= cc < C else 0
-                    if edges == 2:
-                        pass
-                    elif edges == 0:
+                    edges = grid[rr][c] == letter if 0 <= rr < R else 0
+                    edges += grid[r][cc] == letter if 0 <= cc < C else 0
+                    if edges == 0:
                         double_sides += 2
-                    elif edges == corner == 1:
-                        double_sides += 1
-            cost += area * (double_sides//2)
-    return cost
+                    elif edges == 1:
+                        corner = grid[rr][cc] == letter if 0 <= rr < R and 0 <= cc < C else 0
+                        if corner == 1:
+                            double_sides += 1
 
+            p1 += area * perimeter
+            p2 += area * (double_sides // 2)
 
-@aoc_helper.communicator(YEAR, DAY)
-def solve(input_text):
-    return p1(input_text), p2(input_text)
+    return p1, p2
 
 
 if __name__ == "__main__":
