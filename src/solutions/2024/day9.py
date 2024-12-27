e6ea471e7aa32ed9e6cccdd8d30f307e4670c19c
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-27 16:01:10 +0000

    Speed up 2024 day 9

--- src/solutions/2024/day9.py
+++ src/solutions/2024/day9.py
@@ -1,15 +1,13 @@
+import heapq
+
 import aoc_helper
 
 DAY = 9
 YEAR = 2024
 
 
-def check_sum(disk):
-    return sum(pos * file_id for pos, file_id in enumerate(disk) if file_id is not None)
-
-
 def parse_input_p1(input_text):
-    nums = (int(num) for num in input_text)
+    nums = map(int, input_text)
     disk = []
     file = True
     file_id = 0
@@ -28,80 +26,75 @@ def p1(input_text):
 
     total = 0
 
-    index_start = 0
-    index_end = len(disk) - 1
+    forwards_index = 0
+    backwards_index = len(disk) - 1
 
-    while index_start < index_end:
-        while disk[index_end] is None:
-            index_end -= 1
-        while disk[index_start] is not None:
-            total += index_start * disk[index_start]
-            index_start += 1
-        if index_start < index_end:
-            total += index_start * disk[index_end]
-            index_start += 1
-            index_end -= 1
+    while forwards_index < backwards_index:
+        while disk[backwards_index] is None:
+            backwards_index -= 1
+        while disk[forwards_index] is not None:
+            total += forwards_index * disk[forwards_index]
+            forwards_index += 1
+        while forwards_index < backwards_index and disk[backwards_index] is not None and disk[forwards_index] is None:
+            total += forwards_index * disk[backwards_index]
+            forwards_index += 1
+            backwards_index -= 1
 
     return total
 
 
 
 def parse_input_p2(input_text):
-    nums = (int(num) for num in input_text)
-    disk = []
+    nums = map(int, input_text)
+
+    files = []  # file[file_id] = [file_start_index, file_length]
+    gaps = [[] for _ in range(10)]  # gaps[length] = heap of `gap_start_index` for gaps of length length
+
     file = True
-    file_id = 0
+    index = 0
+
     for num in nums:
         if file:
-            disk.append([file_id, num])
-            file_id += 1
-        else:
-            disk.append([None, num])
+            files.append([index, num])
+        elif num > 0:
+            heapq.heappush(gaps[num], index)
+        index += num
         file = not file
-    return disk
+
+    return files, gaps
 
 
 def p2(input_text):
-    disk = parse_input_p2(input_text)
+    files, gaps = parse_input_p2(input_text)
 
-    earliest_gap = [None] * 10
-    for index, item in enumerate(disk):
-        if item[0] is None:
-            for i in range(1, item[1]+1):
-                if earliest_gap[i] is None:
-                    earliest_gap[i] = index
+    max_file_id = (len(input_text) - 1) // 2
+    max_available_gap_length = 9
 
-    file_id = disk[-1][0] if disk[-1][0] is not None else disk[-2][0]
-    index = len(disk) - 1
+    for file_id in range(max_file_id, 0, -1):
+        length = files[file_id][1]
 
-    while file_id > 0:
-        while disk[index][0] != file_id:
-            index -= 1
+        first_gap_index = files[file_id][0]
+        first_gap_len = None
+        for i in range(length, max_available_gap_length + 1):
+             if gaps[i] and gaps[i][0] < first_gap_index:
+                first_gap_index = gaps[i][0]
+                first_gap_len = i
 
-        length = disk[index][1]
-        earliest_index = earliest_gap[length]
-
-        if earliest_index is None:
-            file_id -= 1
-            continue
-        moved = False
-        for new_index in range(earliest_index, index):
-            if disk[new_index][0] is None and disk[new_index][1] >= length:
-                disk[new_index][1] -= length
-                disk.insert(new_index, disk[index])
-                disk[index+1] = [None, length]
-                moved = True
-                earliest_gap[length] = new_index + 1
+        if first_gap_len is None:
+            max_available_gap_length = length - 1
+            if max_available_gap_length == 0:
                 break
-        if not moved:
-            earliest_gap[length] = None
-        file_id -= 1
+        else:
+            index = heapq.heappop(gaps[first_gap_len])
+            files[file_id][0] = index
+            if first_gap_len > length:
+                heapq.heappush(gaps[first_gap_len - length], index + length)
 
-    expanded_disk = []
-    for item in disk:
-        expanded_disk.extend([item[0]] * item[1])
+    check_sum = 0
+    for file_id, (start_index, file_length) in enumerate(files):
+        check_sum += file_id * (2 * start_index + file_length - 1) * file_length // 2
 
-    return check_sum(expanded_disk)
+    return check_sum
 
 
 @aoc_helper.communicator(YEAR, DAY)
