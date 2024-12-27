f63a969e0a7321008a4a0055a6faa8da44a3c52e
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-27 17:09:20 +0000

    Speed up 2024 day 11

--- src/solutions/2024/day11.py
+++ src/solutions/2024/day11.py
@@ -1,7 +1,7 @@
+from collections import Counter, defaultdict
 from functools import cache
 
 import aoc_helper
-from aoc_helper import digit_count
 
 DAY = 11
 YEAR = 2024
@@ -11,36 +11,32 @@ YEAR = 2024
 def blink(stone):
     if stone == 0:
         return 1,
-    d = digit_count(stone)
+    d = len(str(stone))
     if d % 2 == 0:
         return divmod(stone, 10**(d//2))
     return stone * 2024,
 
 
-@cache
-def count_new_stones(stone, blinks):
-    if blinks == 0:
-        return 1
-    new_stones = blink(stone)
-    ans = 0
-    for new_stone in new_stones:
-        ans += count_new_stones(new_stone, blinks-1)
-    return ans
-
-
-def p1(input_text):
-    stones = list(map(int, input_text.split()))
-    return sum(count_new_stones(stone, 25) for stone in stones)
-
-
-def p2(input_text):
-    stones = list(map(int, input_text.split()))
-    return sum(count_new_stones(stone, 75) for stone in stones)
+def count_new_stones(stones, blinks):
+    for _ in range(blinks):
+        new_stones = defaultdict(int)
+        for stone in stones:
+            for new_stone in blink(stone):
+                new_stones[new_stone] += stones[stone]
+        stones = new_stones
+    return stones
 
 
 @aoc_helper.communicator(YEAR, DAY)
 def solve(input_text):
-    return p1(input_text), p2(input_text)
+    stones = Counter(map(int, input_text.split()))
+
+    stones = count_new_stones(stones, 25)
+    p1 = sum(stones.values())
+    stones = count_new_stones(stones, 50)
+    p2 = sum(stones.values())
+
+    return p1, p2
 
 
 if __name__ == "__main__":
