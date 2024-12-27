75dfae534e1c91d3cfbc2967c92ab7a89e0a3d88
blm34 <48328783+blm34@users.noreply.github.com>
2024-12-27 23:02:33 +0000

    Speed up 2024 day 14

--- src/solutions/2024/day14.py
+++ src/solutions/2024/day14.py
@@ -1,6 +1,3 @@
-import re
-from statistics import pvariance as variance
-
 import aoc_helper
 
 DAY = 14
@@ -9,57 +6,70 @@ YEAR = 2024
 R = 103
 C = 101
 
-
 def parse_input(input_text):
-    pattern = r"^p=(\d+),(\d+) v=(-?\d+),(-?\d+)$"
-    robots = re.findall(pattern, input_text, flags=re.MULTILINE)
-    robots = [list(map(int, robot)) for robot in robots]
+    input_text = input_text.replace("p=", "").replace("v=", "")
+    robots = []
+    for line in input_text.splitlines():
+        pos, vel = line.split()
+        posx, posy = pos.split(",")
+        velx, vely = vel.split(",")
+        robots.append([int(posx), int(posy), int(velx), int(vely)])
+
     return robots
 
 
-def step(robots, steps=1):
-    for i, (x, y, vx, vy) in enumerate(robots):
-        robots[i][0] = (x + vx * steps) % C
-        robots[i][1] = (y + vy * steps) % R
-    return robots
-
-
-def p1(input_text):
+@aoc_helper.communicator(YEAR, DAY)
+def solve(input_text):
     robots = parse_input(input_text)
 
-    robots = step(robots, steps=100)
-
+    # Part 1
     quads = [0, 0, 0, 0]
-    for x, y, _, _ in robots:
-        if x == C // 2 or y == R // 2:
+
+    for x, y, vx, vy in robots:
+        x = (x + vx * 100) % C
+        if x == C // 2:
+            continue
+        y = (y + vy * 100) % R
+        if y == R // 2:
             continue
         quad_x = int(x > C // 2)
         quad_y = int(y > R // 2)
         quads[quad_x + 2 * quad_y] += 1
 
-    return quads[0] * quads[1] * quads[2] * quads[3]
+    p1 = quads[0] * quads[1] * quads[2] * quads[3]
 
+    # Part 2
 
-def p2(input_text):
-    robots = parse_input(input_text)
     x_vars = []
     y_vars = []
+
+    n = len(robots)
+
     for time in range(max(R, C)):
-        x_vars.append(variance(robot[0] for robot in robots))
-        y_vars.append(variance(robot[1] for robot in robots))
-        robots = step(robots)
+        sx = 0
+        sxx = 0
+        sy = 0
+        syy = 0
+
+        for i, robot in enumerate(robots):
+            sx += robot[0]
+            sxx += robot[0] ** 2
+            sy += robot[1]
+            syy += robot[1] ** 2
+
+            robots[i][0] = (robots[i][0] + robot[2]) % C
+            robots[i][1] = (robots[i][1] + robot[3]) % R
+
+        x_vars.append((sxx / n) - (sx / n) ** 2)
+        y_vars.append((syy / n) - (sy / n) ** 2)
+
     x_offset = x_vars.index(min(x_vars))
     y_offset = y_vars.index(min(y_vars))
 
-    time = x_offset * R * ((R ** (C - 2)) % C) + y_offset * C * ((C ** (R - 2)) % R)  # By Chinese remainder theorem
-    time = time % (R*C)
+    p2 = (x_offset * R * (pow(R, C - 2, C)) +
+           y_offset * C * (pow(C, R - 2, R))) % (R * C) # By Chinese remainder theorem
 
-    return time
-
-
-@aoc_helper.communicator(YEAR, DAY)
-def solve(input_text):
-    return p1(input_text), p2(input_text)
+    return p1, p2
 
 
 if __name__ == "__main__":
